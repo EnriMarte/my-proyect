b6f62bb636c194c129d3454213410babcc99ecb9
Enrico <enrico.9999@hotmail.com>
2021-11-01 14:47:16 -0300

    prob

--- src/components/AuthDrawer.js
+++ src/components/AuthDrawer.js
@@ -17,7 +17,8 @@ export default class AuthDrawer extends Component{
         this.state = {
             logIn : false,
             user: '',
-            error: ''
+            error: '',
+            hola: ''
         }
     }
 
